7da5d4b44eaf70fcd401cc0c649312327c7036fb
omar2004712 <o.bahutar@gmail.com>
2022-07-28 16:16:51 +0400

    build (driver): built a point schema to save the location of the driver

--- models/driver.js
+++ models/driver.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const pointSchema = new Schema({
+  type: {
+    type: String,
+    default: "Point",
+    coordinates: { type: [Number], index: "2dsphere" },
+  },
+});
+
 const DriverSchema = new Schema({
   email: {
     type: String,
@@ -10,6 +18,7 @@ const DriverSchema = new Schema({
     type: Boolean,
     default: false,
   },
+  location: PointSchema,
 });
 
 const Driver = mongoose.model("driver", DriverSchema);
